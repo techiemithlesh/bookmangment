e63a6b5ff4be64695d4f76f32a834a400e1e6ff2
Mithlesh Kumar <techie.mithlesh@gmail.com>
2025-05-11 21:26:22 +0530

    Day 03 PUT AND DELETE completed

--- src/main/java/com/codewithmithlesh/bookmanagment/controller/BookController.java
+++ src/main/java/com/codewithmithlesh/bookmanagment/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.codewithmithlesh.bookmanagment.controller;
 
+import com.codewithmithlesh.bookmanagment.exception.ResourceNotFoundException;
 import com.codewithmithlesh.bookmanagment.model.Book;
 import com.codewithmithlesh.bookmanagment.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
+
     @PostMapping("/add")
     public ResponseEntity<Book> addbooks(@RequestBody Book book){
 
@@ -26,5 +28,39 @@ public class BookController {
     @GetMapping("/all")
     public ResponseEntity<List<Book>> getAllBooks(){
         return ResponseEntity.ok(bookRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBooksById(@PathVariable Long id){
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Book not found with id " + id));
+
+        return ResponseEntity.ok(book);
+
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Book> updateBookById(@PathVariable Long id, @RequestBody Book bookDetails){
+        Book existingBook = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Book Not found with the given id" + id));
+
+        existingBook.setTitle(bookDetails.getTitle());
+        existingBook.setAuthor(bookDetails.getAuthor());
+        existingBook.setPages(bookDetails.getPages());
+
+        Book updatedBook = bookRepository.save(existingBook);
+
+        return ResponseEntity.ok(updatedBook);
+
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<String> deleteBookById(@PathVariable Long id){
+        Book exisitngBook = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Book not found for given id "));
+        bookRepository.deleteById(id);
+
+        return ResponseEntity.ok("Book with ID " + id + " deleted successfully.");
+
     }
 }
---
+++ src/main/java/com/codewithmithlesh/bookmanagment/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.codewithmithlesh.bookmanagment.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
